59cb1e092ba2d4e3522aa70c592ad46b04e572d8
Brad Cranford <bscranford@gmail.com>
2016-05-13 13:23:10 -0400

    added js function to add ink class to divs on hover event

--- js/default.js
+++ js/default.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
   var grid = 16;
 
   for(i=0; i<(grid*grid); i++) {
-    $("#container").append("<div></div>");
+    $('#container').append('<div></div>');
   };
 
+  $('div').hover(function(){
+    $(this).addClass('ink');
+  });
 });
